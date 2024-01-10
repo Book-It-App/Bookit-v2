06f770f47882a87ecd76c3f61171abe23e49bf0a
Siddharth <jkrish930@gmail.com>
2024-01-10 15:25:30 +0530

    edited nav bar

--- client/src/components/transportBookingSystem/Navbar.js
+++ client/src/components/transportBookingSystem/Navbar.js
@@ -121,13 +121,13 @@ const RenderUser = () => {
               </li>
 
 
-              <li className="text-gray-700 hover:text-gray-900 cursor-pointer text-base lg:text-lg pt-10 md:pt-0 md:ml-5 lg:ml-10">
+              {/* <li className="text-gray-700 hover:text-gray-900 cursor-pointer text-base lg:text-lg pt-10 md:pt-0 md:ml-5 lg:ml-10">
                 <Link to="/transport-booking-system/events">Events</Link>
               </li>
 
               <li className="text-gray-700 hover:text-gray-900 cursor-pointer text-base lg:text-lg pt-10 md:pt-0 md:ml-5 lg:ml-10">
                 <Link to="/transport-booking-system/calendar">Calendar</Link>
-              </li>
+              </li> */}
 
               <li className="text-gray-700 hover:text-gray-900 cursor-pointer text-base lg:text-lg pt-10 md:pt-0 md:ml-5 lg:ml-10">
                 {/* <Link to="/bookings">Bookings</Link> */}
