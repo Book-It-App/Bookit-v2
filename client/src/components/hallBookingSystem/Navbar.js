87cd8b3317371bd0c8c6c6cba3bcdd79e3aca31e
Siddharth <jkrish930@gmail.com>
2024-02-02 10:18:55 +0530

    removed contact

--- client/src/components/hallBookingSystem/Navbar.js
+++ client/src/components/hallBookingSystem/Navbar.js
@@ -133,10 +133,10 @@ const RenderUser = () => {
                 {/* <Link to="/bookings">Bookings</Link> */}
                 <RenderUser/>
               </li>
-
+{/* 
               <li className="text-gray-700 hover:text-gray-900 cursor-pointer text-base lg:text-lg pt-10 md:pt-0 md:ml-5 lg:ml-10">
                 <Link to="/hall-booking-system/contact">Contact</Link>
-              </li>
+              </li> */}
               <li className="text-gray-700 hover:text-gray-900 cursor-pointer text-base lg:text-lg pt-10 md:pt-0 md:ml-5 lg:ml-10">
                 <Link to="/hall-booking-system/profile">Profile</Link>
               </li>
