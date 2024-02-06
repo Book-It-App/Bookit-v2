f6927d3df37afa0c71386244e67a89f56e423ec1
Siddharth <jkrish930@gmail.com>
2024-02-06 16:27:17 +0530

    error resolved

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -124,7 +124,6 @@ const RenderUser = () => {
               {/* <li className="text-gray-700 hover:text-gray-900 cursor-pointer text-base lg:text-lg pt-10 md:pt-0 md:ml-5 lg:ml-10">
                 <Link to="/howtouse">How To Use?</Link>
               </li> */}
-              </li> */}
 {/* 
               <li className="text-gray-700 hover:text-gray-900 cursor-pointer text-base lg:text-lg pt-10 md:pt-0 md:ml-5 lg:ml-10">
                 <Link to="/calendar">Calendar</Link>
@@ -136,7 +135,6 @@ const RenderUser = () => {
 {/* 
               <li className="text-gray-700 hover:text-gray-900 cursor-pointer text-base lg:text-lg pt-10 md:pt-0 md:ml-5 lg:ml-10">
                 <Link to="/contact">Contact</Link>
-              </li> */}
               </li> */}
               <li className="text-gray-700 hover:text-gray-900 cursor-pointer text-base lg:text-lg pt-10 md:pt-0 md:ml-5 lg:ml-10">
                 <Link to="/profile">Profile</Link>
